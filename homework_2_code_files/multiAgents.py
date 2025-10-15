5d1f6c75b72cc37c148ef6436c2202870d3ca3ee
Solomon <solomon.smith9600@coyote.csusb.edu>
2025-10-14 18:01:41 -0700

    Partially finsihed implementation of AI algorithms. Still needs testing

--- homework_2_code_files/multiAgents.py
+++ homework_2_code_files/multiAgents.py
@@ -1,13 +1,13 @@
 from GameStatus_5120 import GameStatus
 
 
-def minimax(game_state: GameStatus, depth: int, maximizingPlayer: bool, alpha=float('-inf'), beta=float('inf')):
-	terminal = game_state.is_terminal()
-	if (depth==0) or (terminal):
-		newScores = game_state.get_scores(terminal)
-		return newScores, None
+def minimax(game_state: GameStatus, depth: int, maximizing_player: bool, alpha=float('-inf'), beta=float('inf')):
+    terminal = game_state.is_terminal()
+    if (depth == 0) or (terminal):
+        newScores = game_state.get_scores(terminal)
+        return newScores, None
 
-	"""
+    """
     YOUR CODE HERE TO FIRST CHECK WHICH PLAYER HAS CALLED THIS FUNCTION (MAXIMIZING OR MINIMIZING PLAYER)
     YOU SHOULD THEN IMPLEMENT MINIMAX WITH ALPHA-BETA PRUNING AND RETURN THE FOLLOWING TWO ITEMS
     1. VALUE
@@ -15,16 +15,43 @@ def minimax(game_state: GameStatus, depth: int, maximizingPlayer: bool, alpha=fl
     
     THE LINE TO RETURN THESE TWO IS COMMENTED BELOW WHICH YOU CAN USE
     """
+    if maximizing_player:
+        maxEval = float('-inf')
+        best_move = None
 
-	# return value, best_move
+        for move in game_state.get_moves():
+            new_state = game_state.get_new_state(move)
+            evaluation = minimax(new_state, depth - 1, False, alpha, beta)[0]
+            if evaluation > maxEval:
+                maxEval = evaluation
+                best_move = move
+            alpha = max(alpha, evaluation)
+            if beta <= alpha:
+                break
+        return maxEval, best_move
+    else:
+        minEval = float('inf')
+        best_move = None
+
+        for move in game_state.get_moves():
+            new_state = game_state.get_new_state(move)
+            evaluation = minimax(new_state, depth - 1, True, alpha, beta)[0]
+            if evaluation < minEval:
+                minEval = evaluation
+                best_move = move
+            beta = min(beta, evaluation)
+            if beta <= alpha:
+                break
+        value = minEval
+        return value, best_move
 
 def negamax(game_status: GameStatus, depth: int, turn_multiplier: int, alpha=float('-inf'), beta=float('inf')):
-	terminal = game_status.is_terminal()
-	if (depth==0) or (terminal):
-		scores = game_status.get_negamax_scores(terminal)
-		return scores, None
+    terminal = game_status.is_terminal()
+    if (depth == 0) or (terminal):
+        scores = game_status.get_negamax_scores(terminal)
+        return scores, None
 
-	"""
+    """
     YOUR CODE HERE TO CALL NEGAMAX FUNCTION. REMEMBER THE RETURN OF THE NEGAMAX SHOULD BE THE OPPOSITE OF THE CALLING
     PLAYER WHICH CAN BE DONE USING -NEGAMAX(). THE REST OF YOUR CODE SHOULD BE THE SAME AS MINIMAX FUNCTION.
     YOU ALSO DO NOT NEED TO TRACK WHICH PLAYER HAS CALLED THE FUNCTION AND SHOULD NOT CHECK IF THE CURRENT MOVE
@@ -32,8 +59,23 @@ def negamax(game_status: GameStatus, depth: int, turn_multiplier: int, alpha=flo
     RETURN THE FOLLOWING TWO ITEMS
     1. VALUE
     2. BEST_MOVE
-    
+
     THE LINE TO RETURN THESE TWO IS COMMENTED BELOW WHICH YOU CAN USE
-    
+
     """
-    #return value, best_move
+    maxEval = float('-inf')
+    best_move = None
+
+    for move in game_status.get_moves():
+        new_state = game_status.get_new_state(move)
+
+        evaluation = -negamax(new_state, depth - 1, -turn_multiplier, -beta, -alpha)[0]
+        if evaluation > maxEval:
+            maxEval = evaluation
+            best_move = move
+        alpha = max(alpha, evaluation)
+        if beta <= alpha:
+            break
+    value = maxEval
+
+    return value, best_move
